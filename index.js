531d89bfdf1c9d1aa252063cc611a7feb24e0936
ensaro <41113027+ensarokumus@users.noreply.github.com>
2024-05-21 16:32:45 +1000

    feat: adds a basic router to the project

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 import http from "http";
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
 const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Hello World!</h1>");
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = http.createServer(async (req, res) => {
+  try {
+    if (req.method === "GET") {
+      let filePath;
+      if (req.url === "/" || req.url === "/index.html") {
+        filePath = path.join(__dirname, "public", "index.html");
+      } else if (req.url === "/about.html") {
+        filePath = path.join(__dirname, "public", "about.html");
+      } else if (req.url === "/contact-me.html") {
+        filePath = path.join(__dirname, "public", "contact-me.html");
+      } else {
+        filePath = path.join(__dirname, "public", "404.html");
+      }
+      const data = await fs.readFile(filePath);
+      res.setHeader("Content-Type", "text/html");
+      res.write(data);
+      res.end();
+    } else {
+      throw new Error("Method not allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("Server Error");
+  }
 });
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
