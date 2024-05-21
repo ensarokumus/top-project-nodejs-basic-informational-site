9da99c189acb222f91dc60c42f24e7d4e7202f57
ensaro <41113027+ensarokumus@users.noreply.github.com>
2024-05-21 16:22:13 +1000

    build: adds nodemon to the project

---
+++ index.js
@@ -0,0 +1,11 @@
+import http from "http";
+const PORT = process.env.PORT;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<h1>Hello World!</h1>");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
